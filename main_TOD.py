17b1693dbb165bcd0299fefbbe96a42ba4e7f8e4
ryan shave <rshave@gmail.com>
2018-12-08 11:46:36 -0600

    retiring linux box :(

--- main_TOD.py
+++ main_TOD.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import time
 import datetime
 import string
@@ -7,22 +8,23 @@ import tweet_with_db
 from bs4 import BeautifulSoup
 
 def main():
+    logging.basicConfig(filename='/home/ryan/github/TOD/tweet.log', level=logging.INFO)
     st = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
     url = "http://fuckinghomepage.com/"
     r = requests.get(url)
     soup = BeautifulSoup(r.content, "html.parser")
-    str = soup.p.text
+    tweet_str = soup.p.text
     #strip off the vulgar text
-    str = str[str.index('.')-len(str)+2:]
-    str = string.capwords(str)
+    tweet_str = tweet_str[tweet_str.index('.') - len(tweet_str) + 2:]
+    tweet_str = string.capwords(tweet_str)
     #now we tweet it
     logging.basicConfig(filename='tweet.log', level=logging.INFO)
-    logging.info('Tweet log for: '+st)
-    tweet_with_db.tweet_it(str)
+    logging.info('Tweet log for: ' + st)
+    tweet_with_db.tweet_it(tweet_str)
     #next we tweet the words of wisdom
     row = soup.find_all('p')[1:7]
-    str = string.capwords(row[1].string)
-    tweet_with_db.tweet_it(str)
+    tweet_str = string.capwords(row[1].string)
+    tweet_with_db.tweet_it(tweet_str)
 
 if __name__ == '__main__':
     main()
